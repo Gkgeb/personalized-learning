20694a48720c7e1803ad56e98c70eb55525acdf0
Bohdan_Herasymov <mestl_zz@ukr.net>
2017-07-04 17:42:53 +0300

    Fix hidden images in tables (#164)

--- app/_components/bindingHandlers/wrapElementsBindingHandler.js
+++ app/_components/bindingHandlers/wrapElementsBindingHandler.js
@@ -17,12 +17,18 @@
                 
                 $("img", element).each(function () {
                     var $self = $(this),
-                        $wrapper = $('<div class="image-wrapper">').css('float', $self.css('float'));
+                        $wrapper = $('<div class="image-wrapper">').css('float', $self.css('float')),
+                        $parent = $self.parent();
 
                     if ($self.closest('.cropped-image').length > 0) {
                         return;
                     }
 
+                    if ($parent.prop('tagName') == "TD" && $parent[0].style.width == "") {
+                        $wrapper.css('width', $image[0].style.width);
+                        $wrapper.css('height', $image[0].style.height);
+                    }
+                    
                     $self.wrap($wrapper);
                     $self.height('auto');
                 });
